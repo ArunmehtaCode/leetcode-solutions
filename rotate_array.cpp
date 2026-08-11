2ee4fdcff7b1f0ae704ba6451a797afa0b4b4565
arunmehtacode <arunmehta2812@gmail.com>
2026-08-11 18:37:47 +0530

    rotate

---
+++ rotate_array.cpp
@@ -0,0 +1,10 @@
+              //reverse and rotate are completely different
+              // ler say int arr[]={1,2,3,4,5};
+              // reverse is = [5,4,3,2,1] and rotate by 1 is [5,1,2,3,4] , rotate by 2 is [4,5,1,2,3]
+              //and even if we reverse only ist and last elemnt still it will give [5,2,3,4,1] which is not rotaed by 1
+#include<iostream>
+using namespace std;
+int main(){
+
+}
+//swap(arr[n-1],arr[n-2])  more cleaner way just simple
