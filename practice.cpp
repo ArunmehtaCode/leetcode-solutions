afe23c898fe4320f629e6e40a92caa8528674339
arunmehtacode <arunmehta2812@gmail.com>
2026-08-17 22:52:29 +0530

    practice code

--- practice.cpp
+++ practice.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
+    int arr[]={1,4,3,1,5,4,0,9};
+    int n=8;
+    for(int i=0;i<n-1;i++){
+        int minIdx = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minIdx]){
+                minIdx = j;
+            }
+        }
+        swap(arr[i],arr[minIdx]);
+    }
+    for( int num : arr){
+        cout<<num<<" ";
+    }
+
     
 }
