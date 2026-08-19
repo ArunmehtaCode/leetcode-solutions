1f388fd45d0bd948feb938e32e8b280fe184332d
arunmehtacode <arunmehta2812@gmail.com>
2026-08-20 02:43:26 +0530

    reverse integer

---
+++ reverse_integer.cpp
@@ -0,0 +1,19 @@
+   //7. Reverse Integer
+//Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the 
+//signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// Example 1:
+// Input: x = 123
+// Output: 321
+
+#include<iostream>
+using namespace std;
+int main(){
+   int n;
+   cout<<"enter a number to reverse";
+   cin>>n;
+   while(n!=0){
+      
+   }
+   
+}
