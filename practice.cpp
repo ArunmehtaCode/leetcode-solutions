d0bd1d59d85617975525b6ff636fe7644dfafee7
arunmehtacode <arunmehta2812@gmail.com>
2026-08-20 21:11:34 +0530

    practice code

--- practice.cpp
+++ practice.cpp
@@ -1,37 +1,33 @@
 #include<iostream>
-
 using namespace std;
 
-int binarySearch(int arr[], int n, int t) {
+int binarySearch(int arr[], int n, int target) {
 
-	int s = 0;
-	int e = n - 1;
+	int s = 0;  //start 
+	int e = n - 1; //end
 
 	while (s <= e) {
 
-		// int m = (s + e) / 2;
-		int m = s + (e - s) / 2;
-		if (arr[m] == t) {
-			// you've found the target at index m
-			return m;
-		} else if (t > arr[m]) {
+		// int m = (s + e) / 2;  // this is correct but for some worst cases it will crash meand s and e are basically integer value and
+	// let say at some point both s and e has int_max value or sum of both exceed INT_MAX then it will overflow and give wrong answer
+		int mid = s + (e - s) / 2; 
+	    if (target > arr[mid]) {
 			// search for the target to the right
-			// of the midPoint i.e. search space
-			// reduces from [s, e] to [m+1, e]
-			s = m + 1;
-		} else {
-			// t < arr[m]
+			// of the midPoint i.e. search space reduces from [s, e] to [m+1, e]
+			s = mid + 1;
+		} else if (target < arr[mid]){
 			// search for the target to the left
-			//  of the midPoint i.e. search space
-			// reduces from [s, e] to [s, m-1]
-			e = m - 1;
+			//  of the midPoint i.e. search space reduces from [s, e] to [s, m-1]
+			e = mid - 1;
 		}
-
+		else{
+			// arr[m] = target
+			return mid;
+		}
 	}
 
-	// s > e i.e. target was not found
+	// s > e and nothing return means target was not found
 	return -1;
-
 }
 
 int main() {
@@ -39,7 +35,7 @@ int main() {
 	int arr[] = {10, 20, 30, 40, 50, 60, 70};
 	int n = sizeof(arr) / sizeof(int);
 
-	int t = 20;
+	int t = 60;
 
 	cout << binarySearch(arr, n, t) << endl;
 
