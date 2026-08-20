e551a2981e9737ab43a661c505a240bf0b7df770
arunmehtacode <arunmehta2812@gmail.com>
2026-08-21 02:01:50 +0530

    first occurence in sorted arrray

---
+++ Sorting_algorithms/first_occurence_insortedarray.cpp
@@ -0,0 +1,29 @@
+  //First Occurrence in Sorted Array 
+#include<iostream>
+using namespace std;
+int first_occcur(int arr[], int n , int target){
+    int start = 0;
+    int end = n-1;
+    int ans=-1;
+    while(start<=end){
+        int mid = start + (end-start)/2;
+        if(arr[mid]>target){
+            end = mid-1;
+        }
+        else if(arr[mid]<target){
+            start = mid+1;
+        }
+        else{
+             ans =mid;
+             end = mid-1;
+
+        }
+    }
+    return ans;
+}
+int main(){
+    int arr[] = {1,2,5,5,5,5,6,6,7,9};
+    int n = sizeof(arr)/sizeof(int);
+    int target = 6;
+    cout<< first_occcur(arr,n,target);
+}
