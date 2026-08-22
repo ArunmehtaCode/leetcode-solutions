a7089727cdc8538ffc47d647e14e027f7d770923
arunmehtacode <arunmehta2812@gmail.com>
2026-08-22 23:55:40 +0530

    practice

--- practice.cpp
+++ practice.cpp
@@ -32,7 +32,7 @@ int binarySearch(int arr[], int n, int target) {
 
 int main() {
 
-	int arr[] = {10, 20, 30, 40, 50, 60, 70};
+	int arr[] = {10, 20, 30, 40, 50, 60, 70}; 
 	int n = sizeof(arr) / sizeof(int);
 
 	int t = 60;
